46f0697f878d087810b89e7c3894f807e4b0f502
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-08-30 22:44:45 -0300

    set relationships between user and model up

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  has_many :posts
+
   validates :username, presence: true, uniqueness: true, length: { minimum: 4, maximum: 12}
   validates :email, presence: true, uniqueness: true
   validates :password, presence: true
