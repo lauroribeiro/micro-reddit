ad9095c1095e07fb6958b81786a9930f30784de2
Lauro Ribeiro <lauroribeirola@hotmail.com>
2021-08-30 23:28:49 -0300

    create migration to rename wrong foreing keys

--- db/schema.rb
+++ db/schema.rb
@@ -10,16 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_08_31_015446) do
+ActiveRecord::Schema.define(version: 2021_08_31_022602) do
 
   create_table "comments", force: :cascade do |t|
     t.text "body"
-    t.integer "posts_id"
-    t.integer "users_id"
+    t.integer "post_id"
+    t.integer "user_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.index ["posts_id"], name: "index_comments_on_posts_id"
-    t.index ["users_id"], name: "index_comments_on_users_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
   end
 
   create_table "posts", force: :cascade do |t|
